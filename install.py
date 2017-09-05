f10b074cd9d88fd2ebf9d619fb16dccfcf776cca
olliecad1 <officialgamermorris@gmail.com>
2017-09-05 18:18:12 +0000

    new update

--- install.py
+++ install.py
@@ -2,10 +2,11 @@ import subprocess
 
 subprocess.call('sudo git clone https://github.com/petertodd/python-bitcoinlib', shell=True)
 
-
 subprocess.call('sudo python setup.py build', shell=True, cwd="python-bitcoinlib")
-
 
 subprocess.call('sudo python setup.py install', shell=True, cwd="python-bitcoinlib")
 
+subprocess.call('sudo python3 setup.py build', shell=True, cwd="python-bitcoinlib")
+
+subprocess.call('sudo python3 setup.py install', shell=True, cwd="python-bitcoinlib")
 
